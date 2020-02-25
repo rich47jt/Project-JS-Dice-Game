21b335dfbeefe684b16b393b6f4106398bedc90d
unknown <theusjt49@gmail.com>
2020-02-25 15:23:35 -0600

    created css style sheet and Dice method in js

--- Java Scritpt.js
+++ Java Scritpt.js
@@ -1,13 +1,27 @@
-
-
-function player()
-{ 
-console.log ("Wanna play");
-
-document.getElementById("Java").innerHTML=player()
+function MakePlayer()
+{
+    let player = {FirstName,Dice};
+    Console.log("Wanna Play")
+    let reponse = prompt("true or false");
+    if (reponse === "true")
+    {
+        player = new player;
+        document.getElementById("Java").innerHTML = "Lets Start the game!";
+    }
 }
 
+function DiceShootOut()
+{
+  let Dice = console.log(Math.floor(Math.random()*20)+1);
+    
+    if(Dice > Dice)
+    {
+        console.log("You have won");
+    }
+    if (Dice < Dice)
+    {
+        console.log("You Lost!")
+        .Remove(player)
+    }
+}
 
-
-
-// console.log(Math.round(Math.round())*20)

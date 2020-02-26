ad53017494022a83555cc633c242d2d07d27d296
unknown <theusjt49@gmail.com>
2020-02-26 08:01:43 -0600

    Created list of players

--- Java Scritpt.js
+++ Java Scritpt.js
@@ -1,27 +1,39 @@
 function MakePlayer()
 {
-    let player = {FirstName,Dice};
+    let player = {FirstName,Dice};//eed way to take input on player info
+    //how do i set the limit of players 
     Console.log("Wanna Play")
     let reponse = prompt("true or false");
     if (reponse === "true")
     {
         player = new player;
-        document.getElementById("Java").innerHTML = "Lets Start the game!";
+        response = document.getElementById("Java").innerHTML = "Lets Start the game!";
     }
+    return reponse//need to return a msg to the player
 }
 
-function DiceShootOut()
-{
-  let Dice = console.log(Math.floor(Math.random()*20)+1);
-    
-    if(Dice > Dice)
-    {
-        console.log("You have won");
-    }
-    if (Dice < Dice)
-    {
-        console.log("You Lost!")
-        .Remove(player)
-    }
-}
+// function WinDiceShootOut()
+// {
+//     var R = document.getElementsByName("mySelect");
+//   console.log(Math.floor(Math.random()*20)+1); // is this really randomizing my roll?
+//   if(player.Dice > player.Dice) // need to distinguish btwn players
+//   {
+//    console.log("You have won this round");
+//   }
+//    if (player.Dice < player.Dice)
+//    {
+//        console.log("You have lost this round")
+//        R.remove(R.player);
+//        //need to remove player 
+//    }
+//    else if (player.Dice === player.Dice)
+//    {
+//     console.log(Math.floor(Math.random()*20)+1);   
+//     //redo roll
+//    }
+
+
+//}
+
+
 
